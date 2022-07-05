2955c7f81e3afaa187b56f8ae7c00c5e16caa5df
ichi66 <clu3842@gmail.com>
2022-07-05 22:58:45 +0800

    fix: fix ignorecase issue

---
+++ src/styledComponents/FlexWrapper.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const VerticalFlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HorizontalFlexWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
